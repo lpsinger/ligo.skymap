3515aeea57b212ac96df3282097cd8cbe0686137
Leo Singer <leo.singer@ligo.org>
2018-02-06 11:25:08 -0500

    Bundle chealpix sources

--- ligo/skymap/setup_package.py
+++ ligo/skymap/setup_package.py
@@ -1,23 +1,35 @@
-from astropy_helpers.openmp_helpers import add_openmp_flags_if_available
-from astropy_helpers.setup_helpers import pkg_config
+from astropy_helpers import openmp_helpers, setup_helpers
 from distutils.core import Extension
 
 
 def get_extensions():
-    kwargs = pkg_config(['chealpix', 'gsl'], [])
-    kwargs['include_dirs'].extend(['numpy'])
+    pkg_config_packages = ['gsl']
+
+    sources = [
+        'src/cubic_interp.c',
+        'src/bayestar_moc.c',
+        'src/core.c',
+        'src/bayestar_distance.c',
+        'src/bayestar_sky_map.c']
+
+    include_dirs = ['numpy']
+
+    if setup_helpers.use_system_library('chealix'):
+        pkg_config_packages.append('chealpix')
+    else:
+        include_dirs.append('cextern/chealpix')
+        sources.append('cextern/chealpix/chealpix.c')
+
+    kwargs = setup_helpers.pkg_config(pkg_config_packages, [])
+    kwargs['include_dirs'].extend(include_dirs)
 
     extension = Extension(
-        name='ligo.skymap.core',
-        language='c',
-        sources=[
-            'src/cubic_interp.c',
-            'src/bayestar_moc.c',
-            'src/core.c',
-            'src/bayestar_distance.c',
-            'src/bayestar_sky_map.c'],
-        **kwargs)
+        name='ligo.skymap.core', language='c', sources=sources, **kwargs)
 
-    add_openmp_flags_if_available(extension)
+    openmp_helpers.add_openmp_flags_if_available(extension)
 
     return [extension]
+
+
+def get_external_libraries():
+    return ['chealpix']

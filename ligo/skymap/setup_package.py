4897d353b9c6647d2b7e3596e91f5ef95db40ac9
Leo Singer <leo.singer@ligo.org>
2018-03-01 09:42:07 -0500

    Fix typo

--- ligo/skymap/setup_package.py
+++ ligo/skymap/setup_package.py
@@ -23,7 +23,7 @@ def get_extensions():
 
     kwargs = setup_helpers.pkg_config(pkg_config_packages, [])
     kwargs['include_dirs'].extend(include_dirs)
-    kwargs['extra_compile_args'].extend(['-std=gnu99' '-DGSL_RANGE_CHECK_OFF'])
+    kwargs['extra_compile_args'].extend(['-std=gnu99', '-DGSL_RANGE_CHECK_OFF'])
 
     extension = Extension(
         name='ligo.skymap.core', language='c', sources=sources, **kwargs)

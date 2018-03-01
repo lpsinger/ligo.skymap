ade3e7142743e323581e61ffde040617ad4974f6
Leo Singer <leo.singer@ligo.org>
2018-03-01 09:16:20 -0500

    Remove doc comment

--- ligo/skymap/postprocess/ellipse.py
+++ ligo/skymap/postprocess/ellipse.py
@@ -16,9 +16,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-"""
-Postprocessing utilities for HEALPix sky maps
-"""
 
 from astropy.wcs import WCS
 import healpy as hp

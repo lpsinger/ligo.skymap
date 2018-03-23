5847e2a0e16cf7d23f91ea8c102b751bf5e0031e
Leo Singer <leo.singer@ligo.org>
2018-03-23 16:16:13 -0700

    Update copyright

--- ligo/skymap/bayestar/filter.py
+++ ligo/skymap/bayestar/filter.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 #
-# Copyright (C) 2013-2015  Leo Singer
+# Copyright (C) 2013-2018  Leo Singer
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the

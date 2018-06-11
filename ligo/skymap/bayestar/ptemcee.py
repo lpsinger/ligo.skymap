6667d567e74497fefd1a30c14f55898fddc58c54
Leo Singer <leo.singer@ligo.org>
2018-06-11 19:07:06 -0400

    Add FIXME comment [ci skip]

--- ligo/skymap/bayestar/ptemcee.py
+++ ligo/skymap/bayestar/ptemcee.py
@@ -1,5 +1,8 @@
 # Copyright (C) 2018  Leo Singer
 #
+# FIXME: Remove this file if https://github.com/willvousden/ptemcee/pull/6
+# is merged
+#
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
 # Free Software Foundation; either version 2 of the License, or (at your

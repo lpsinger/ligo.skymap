be7c31c3b209b66058a3542434805d30ecba11d2
Leo Singer <leo.singer@ligo.org>
2018-08-27 15:35:54 -0400

    Exercise ligo-skymap-plot-volume

--- ligo/skymap/tool/tests/test_plot.py
+++ ligo/skymap/tool/tests/test_plot.py
@@ -1,20 +1,44 @@
 import numpy as np
 import pytest
 
+from ... import distance
 from ... import io
 from . import run_entry_point
 
 
+@pytest.fixture
+def skymap(tmpdir):
+    npix = 12
+    filename = str(tmpdir / 'skymap.fits')
+    distmean = 100.0
+    diststd = 20.0
+    distmu, distsigma, distnorm = distance.moments_to_parameters(
+        distmean, diststd)
+    io.write_sky_map(filename,
+                     (np.tile(1 / npix, npix),
+                      np.tile(distmu, npix),
+                      np.tile(distsigma, npix),
+                      np.tile(distnorm, npix)),
+                     gps_time=1e9, objid='foobar',
+                     distmean=distmean, diststd=diststd)
+    return filename
+
+
 @pytest.mark.parametrize('geo', [False, True])
-def test_plot(tmpdir, geo):
+def test_plot(tmpdir, skymap, geo):
     """Test ligo-skymap-plot."""
-    fitsfilename = str(tmpdir / 'skymap.fits')
     pngfilename = str(tmpdir / 'skymap.png')
-    io.write_sky_map(fitsfilename, np.arange(12) / 12,
-                     gps_time=1e9, objid='foobar')
-    args = ['ligo-skymap-plot', fitsfilename, '-o', pngfilename, '--annotate',
+    args = ['ligo-skymap-plot', skymap, '-o', pngfilename, '--annotate',
             '--colorbar', '--radec', '320', '45']
     if geo:
         args.append('--geo')
     args.extend(['--contour', '90'])
+    run_entry_point(*args)
+
+
+def test_plot_volume(tmpdir, skymap):
+    """Test ligo-skymap-plot-volume (at super-low resolution for speed)."""
+    pngfilename = str(tmpdir / 'skymap3d.png')
+    args = ['ligo-skymap-plot-volume', skymap, '-o', pngfilename, '--dpi', '8',
+            '--annotate', '--contour', '90', '--radecdist', '320', '45', '100']
     run_entry_point(*args)

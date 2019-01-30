ae5f3edb34e3b464bae8603ba12122895109e710
Leo Singer <leo.singer@ligo.org>
2019-01-30 18:53:05 -0500

    Reuse array_function_dispatch from Numpy

--- ligo/skymap/extern/quantile.py
+++ ligo/skymap/extern/quantile.py
@@ -34,7 +34,7 @@
 import warnings
 
 import numpy as np
-from numpy.lib.function_base import _ureduce
+from numpy.lib.function_base import _ureduce, array_function_dispatch
 
 
 def _percentile_dispatcher(a, q, axis=None, weights=None, out=None,

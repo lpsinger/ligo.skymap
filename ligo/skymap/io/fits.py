570f8dcc78e6eefe52f944590f0a4ca719403738
Leo Singer <leo.singer@ligo.org>
2019-07-03 10:04:03 -0400

    Update copyright [ci skip]

--- ligo/skymap/io/fits.py
+++ ligo/skymap/io/fits.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2013-2017  Leo Singer
+# Copyright (C) 2013-2019  Leo Singer
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the

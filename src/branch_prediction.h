f8534845f4a0a7aa0b177de3f3e2538120058217
Leo Singer <leo.singer@ligo.org>
2019-08-07 14:54:01 -0700

    Use !! idiom in branch prediction hint to convert to bool

--- src/branch_prediction.h
+++ src/branch_prediction.h
@@ -3,7 +3,7 @@
 #ifndef BRANCH_PREDICTION_H
 #define BRANCH_PREDICTION_H
 
-#define LIKELY(x) __builtin_expect ((x), 1)
-#define UNLIKELY(x) __builtin_expect ((x), 0)
+#define LIKELY(x) __builtin_expect(!!(x), 1)
+#define UNLIKELY(x) __builtin_expect(!!(x), 0)
 
 #endif /* BRANCH_PREDICTION_H */

587bdbbae5c192e409b17aa8882357b6de4e4a53
Leo Singer <leo.singer@ligo.org>
2020-04-23 11:34:16 -0400

    More unit tests

--- ligo/skymap/postprocess/tests/test_crossmatch.py
+++ ligo/skymap/postprocess/tests/test_crossmatch.py
@@ -1,6 +1,9 @@
+from astropy.coordinates import SkyCoord, SphericalRepresentation
 from astropy.table import Table
+from astropy import units as u
 import numpy as np
 import healpy as hp
+import pytest
 from scipy import stats
 
 from ...distance import cartesian_kde_to_moments, moments_to_parameters
@@ -8,43 +11,75 @@ from ...moc import nest2uniq
 from ..crossmatch import crossmatch
 
 
-def test_crossmatch():
-    # Construct a random mean vector and covariance matrix
-    # for a multivariate Gaussian distribution in Cartesian coordinates.
+@pytest.fixture
+def cartesian_gaussian():
+    """Generate a 3D Cartesian Gaussian with random mean and covariance."""
     rotation = stats.special_ortho_group(3).rvs()
     eigenvalues = stats.gamma(2).rvs(3)
-    cart_inv_cov = rotation @ np.diag(1 / eigenvalues) @ rotation.T
-    cart_mean = stats.norm.rvs(size=3)
+    cov = rotation @ np.diag(eigenvalues) @ rotation.T
+    mean = stats.norm.rvs(size=3)
+    return stats.multivariate_normal(mean, cov)
 
+
+def cartesian_gaussian_to_skymap(level, mean, cov):
+    """Convert a 3D Cartesian Gaussian to a 3D sky map."""
     # Set up HEALPix grid.
-    level = 5
     nside = 2**level
     npix = hp.nside2npix(nside)
     ipix = np.arange(npix)
-    cart_coords = np.column_stack(hp.pix2vec(nside, ipix, nest=True))
+    coords = np.column_stack(hp.pix2vec(nside, ipix, nest=True))
 
-    # Create sky map using same method as KDE.
-    cart_means = cart_mean[np.newaxis, ..., np.newaxis]
-    cart_inv_covs = cart_inv_cov[np.newaxis, ...]
+    # Create sky map using same method as KDE to convert from Cartesian
+    # to spherical representation. This is just a special case where there
+    # is only a single cluster and a single KDE sample.
+    means = mean[np.newaxis, ..., np.newaxis]
+    inv_covs = np.linalg.inv(cov)[np.newaxis, ...]
     weights = np.ones(1)
     probdensity, distmean, diststd = np.transpose([
-        cartesian_kde_to_moments(n, cart_means, cart_inv_covs, weights)
-        for n in cart_coords])
+        cartesian_kde_to_moments(n, means, inv_covs, weights) for n in coords])
 
     # Create 3D, multi-order sky map.
     uniq = nest2uniq(level, ipix)
     distmu, distsigma, distnorm = moments_to_parameters(distmean, diststd)
-    skymap = Table({'UNIQ': uniq,
-                    'PROBDENSITY': probdensity,
-                    'DISTMU': distmu,
-                    'DISTSIGMA': distsigma,
-                    'DISTNORM': distnorm})
+    return Table({'UNIQ': uniq,
+                  'PROBDENSITY': probdensity,
+                  'DISTMU': distmu,
+                  'DISTSIGMA': distsigma,
+                  'DISTNORM': distnorm})
 
-    # Run function under test.
-    contours = [0.1, 0.5, 0.9]
-    result = crossmatch(skymap, contours=contours)
+
+@pytest.fixture(params=range(3))
+def contours(request):
+    """Generate random values for the ``contours`` argument."""
+    return np.random.uniform(size=request.param)
+
+
+@pytest.mark.parametrize('n_coordinates', [None, *range(3)])
+def test_crossmatch_cartesian_gaussian_distribution(
+        cartesian_gaussian, contours, n_coordinates):
+    """Test on a Cartesian Gaussian distribution."""
+    skymap = cartesian_gaussian_to_skymap(
+        5, cartesian_gaussian.mean, cartesian_gaussian.cov)
+
+    if n_coordinates is None:
+        coordinates = None
+    else:
+        coordinates_xyz = cartesian_gaussian.rvs(size=n_coordinates)
+        coordinates = SkyCoord(*coordinates_xyz.T * u.Mpc,
+                               representation_type='cartesian')
+
+    result = crossmatch(skymap, contours=contours, coordinates=coordinates)
 
     # Test credible volumes against the known analytic expression.
     expected = (4 / 3 * np.pi * stats.chi(3).ppf(contours)**3 *
-                np.sqrt(np.prod(eigenvalues)))
+                np.sqrt(np.linalg.det(cartesian_gaussian.cov)))
     np.testing.assert_allclose(result.contour_vols, expected, rtol=1e-3)
+
+    if coordinates is None:
+        assert np.isnan(result.probdensity_vol)
+    elif np.size(coordinates) == 0:
+        assert np.size(result.probdensity_vol) == 0
+    else:
+        expected = cartesian_gaussian.pdf(coordinates_xyz)
+        np.testing.assert_allclose(result.probdensity_vol, expected,
+                                   rtol=1e-1)

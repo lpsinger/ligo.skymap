9227fe7bf522cf8095ad6012a31a983deca18dc9
Leo Singer <leo.singer@ligo.org>
2020-05-05 15:51:53 -0400

    Add unit test for dVC_dVL_for_DL and dVC_dVL_for_z

---
+++ ligo/skymap/postprocess/tests/test_cosmology.py
@@ -0,0 +1,31 @@
+from astropy import units as u
+import numpy as np
+import pytest
+
+from ..cosmology import cosmo, dVC_dVL_for_DL, dVC_dVL_for_z
+
+
+@pytest.mark.parametrize('z', np.logspace(-6, 2))
+def test_dVC_dVL(z):
+    dVC_dz = cosmo.differential_comoving_volume(z)
+
+    DH = cosmo.hubble_distance
+    DL = cosmo.luminosity_distance(z)
+    DM = cosmo.comoving_transverse_distance(z)
+
+    dDC_dz = DH * cosmo.inv_efunc(z)  # DC = integral of inv_efunc
+
+    assert cosmo.Ok0 == 0  # Flat universe
+    dDM_dz = dDC_dz  # otherwise this expression is more complicated
+
+    dDL_dz = DM + (1 + z) * dDM_dz  # DL = (1 + z) DM
+
+    dVL_dz = DL**2 * dDL_dz * u.sr**-1  # by definition
+
+    expected = (dVC_dz / dVL_dz).to_value(u.dimensionless_unscaled)
+
+    result = dVC_dVL_for_z(z)
+    assert expected == pytest.approx(result)
+
+    result = dVC_dVL_for_DL(DL.to_value(u.Mpc))
+    assert expected == pytest.approx(result)

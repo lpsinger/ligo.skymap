2866d5e4d88dfd6805e8b92873cb4d37b369b796
Leo Singer <leo.singer@ligo.org>
2020-05-25 10:24:26 -0400

    Update Celery intersphinx link to https

--- docs/conf.py
+++ docs/conf.py
@@ -225,7 +225,7 @@ modindex_common_prefix = [package.__name__ + '.']
 intersphinx_mapping.update({
     'gracedb': ('https://gracedb.ligo.org/documentation/', None),
     'gwcelery': ('https://gw.readthedocs.io/projects/gwcelery/en/latest/', None),
-    'celery': ('http://docs.celeryproject.org/en/latest/', None),
+    'celery': ('https://docs.celeryproject.org/en/latest/', None),
     'ligo-lw': ('https://docs.ligo.org/kipp.cannon/python-ligo-lw/', None),
     'gwpy': ('https://gwpy.github.io/docs/latest/', None)
 })

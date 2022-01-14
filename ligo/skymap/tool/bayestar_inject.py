a90f87f8fdf7ba88a6820080abac08da0b23b282
Leo Singer <leo.singer@ligo.org>
2022-01-14 14:02:25 -0500

    Update copyright messages

--- ligo/skymap/tool/bayestar_inject.py
+++ ligo/skymap/tool/bayestar_inject.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2020  Leo Singer
+# Copyright (C) 2019-2022  Leo Singer
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

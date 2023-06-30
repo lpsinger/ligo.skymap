f9f3efdf86a4a99323f60cd4f5855a16433e5c48
Leo Singer <leo.singer@ligo.org>
2023-06-30 15:28:38 -0400

    Update copyright dates

--- ligo/skymap/tool/ligo_skymap_contour_moc.py
+++ ligo/skymap/tool/ligo_skymap_contour_moc.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2013-2020 Giuseppe Greco, Leo Singer, and CDS team.
+# Copyright (C) 2013-2023 Giuseppe Greco, Leo Singer, and CDS team.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

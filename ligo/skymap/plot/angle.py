4ce655be6eb4f59fba896f7e53484945f633364c
Leo Singer <leo.singer@ligo.org>
2024-04-10 00:12:56 -0400

    Add doctests for ligo.skymap.plot.angle

--- ligo/skymap/plot/angle.py
+++ ligo/skymap/plot/angle.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2012-2020  Leo Singer
+# Copyright (C) 2012-2024  Leo Singer
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,22 +22,50 @@ __all__ = ('reference_angle', 'reference_angle_deg',
 
 
 def reference_angle(a):
-    """Convert an angle to a reference angle between -pi and pi."""
+    """Convert an angle to a reference angle between -pi and pi.
+
+    Examples
+    --------
+
+    >>> reference_angle(1.5 * np.pi)
+    array(-1.57079633)
+    """
     a = np.mod(a, 2 * np.pi)
     return np.where(a <= np.pi, a, a - 2 * np.pi)
 
 
 def reference_angle_deg(a):
-    """Convert an angle to a reference angle between -180 and 180 degrees."""
+    """Convert an angle to a reference angle between -180 and 180 degrees.
+
+    Examples
+    --------
+
+    >>> reference_angle_deg(270.)
+    array(-90.)
+    """
     a = np.mod(a, 360)
     return np.where(a <= 180, a, a - 360)
 
 
 def wrapped_angle(a):
-    """Convert an angle to a reference angle between 0 and 2*pi."""
+    """Convert an angle to a reference angle between 0 and 2*pi.
+
+    Examples
+    --------
+
+    >>> wrapped_angle(3 * np.pi)
+    3.141592653589793
+    """
     return np.mod(a, 2 * np.pi)
 
 
 def wrapped_angle_deg(a):
-    """Convert an angle to a reference angle between 0 and 2*pi."""
+    """Convert an angle to a reference angle between 0 and 2*pi.
+
+    Examples
+    --------
+
+    >>> wrapped_angle_deg(540.)
+    180.0
+    """
     return np.mod(a, 360)

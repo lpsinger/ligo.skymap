67b448aec41e06f8fc2bae32c8626b22da58cffc
Ethan Jacob Marx <ethan.marx@ligo.org>
2025-03-05 16:00:25 -0800

    add license to dpgmm.py

--- ligo/skymap/dpgmm.py
+++ ligo/skymap/dpgmm.py
@@ -1,3 +1,24 @@
+#
+# Copyright (C) 2012-2025  Ethan Marx <emarx@mit.edu>
+#                          Leo P. Singer <leo.singer@ligo.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from figaro.mixture import DPGMM
+from figaro.utils import get_priors
+from .coordinates import EigenFrame
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 from figaro.mixture import DPGMM

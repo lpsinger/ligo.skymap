bc49d148bc170d64298fcc970ba61ae464165155
Leo Singer <leo.singer@ligo.org>
2025-05-27 08:47:38 -0400

    Modernize setuptools_scm version file

--- setup.py
+++ setup.py
@@ -66,20 +66,7 @@ def get_extensions():
     return [extension]
 
 
-VERSION_TEMPLATE = """
-# Note that we need to fall back to the hard-coded version if either
-# setuptools_scm can't be imported or setuptools_scm can't determine the
-# version, so we catch the generic 'Exception'.
-try:
-    from setuptools_scm import get_version
-    version = get_version(root='..', relative_to=__file__)
-except Exception:
-    version = '{version}'
-""".lstrip()
-
-
-use_scm_version = {'write_to': 'ligo/skymap/version.py',
-                   'write_to_template': VERSION_TEMPLATE}
+use_scm_version = {}
 
 # If we are building under the GitLab CI pipeline and we are building on the
 # default branch, then disable the local part of the version
